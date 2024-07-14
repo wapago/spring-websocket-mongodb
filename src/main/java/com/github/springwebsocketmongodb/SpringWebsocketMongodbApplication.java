fb50effcd48309c9a74bc469375c7e2584ccbb94
Kim Chanho <beroso20th@gmail.com>
2024-07-15 01:04:31 +0900

    first commit

---
+++ src/main/java/com/github/springwebsocketmongodb/SpringWebsocketMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.github.springwebsocketmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/springwebsocketmongodb/SpringWebsocketMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.springwebsocketmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
