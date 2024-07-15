f63c452e408a01d2228d55589501d7b94392ec12
Kim Chanho <beroso20th@gmail.com>
2024-07-16 01:14:09 +0900

    Feat: chat 알림,전송 완료

---
+++ src/main/java/com/github/springwebsocketmongodb/chat/ChatMessageService.java
@@ -0,0 +1,31 @@
+package com.github.springwebsocketmongodb.chat;
+
+import com.github.springwebsocketmongodb.chatroom.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+    private final ChatMessageRepository repository;
+    private final ChatRoomService chatRoomService;
+
+    public ChatMessage save(ChatMessage chatMessage) {
+        var chatId = chatRoomService.getChatRoomId(chatMessage.getSenderId(), chatMessage.getRecipientId(), true)
+                .orElseThrow(); // you can create your own dedicated exception
+
+        chatMessage.setChatId(chatId);
+        repository.save(chatMessage);
+        return chatMessage;
+    }
+
+    // not create new chatroom
+    public List<ChatMessage> findChatMessages(String senderId, String recipientId) {
+        var chatId = chatRoomService.getChatRoomId(senderId, recipientId, false);
+
+        return chatId.map(repository::findByChatId).orElse(new ArrayList<>());
+    }
+}
