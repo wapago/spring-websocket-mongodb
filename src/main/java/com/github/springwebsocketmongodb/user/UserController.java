cbb00e33fbce3667a6ae1d2f6a445490c4c17690
Kim Chanho <beroso20th@gmail.com>
2024-07-20 23:51:06 +0900

    Feat: user connect/disconnect 적용, 로그 적용

--- src/main/java/com/github/springwebsocketmongodb/user/UserController.java
+++ src/main/java/com/github/springwebsocketmongodb/user/UserController.java
@@ -1,6 +1,8 @@
 package com.github.springwebsocketmongodb.user;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,20 +12,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
     @MessageMapping("/user.addUser")
-    @SendTo("/user/topic")
+    @SendTo("/user/public")
     public User addUser(
-            @Payload User user
+            @Payload User user,
+            HttpServletRequest request
     ) {
+        log.info(request.getRequestURI());
+        log.info("user = " + user);
         userService.saveUser(user);
         return user;
     }
 
+    @MessageMapping("/user.disconnectUser")
+    @SendTo("/user/public")
     public User disconnect(
             @Payload User user
     ) {
--- src/main/java/com/github/springwebsocketmongodb/user/UserService.java
+++ src/main/java/com/github/springwebsocketmongodb/user/UserService.java
@@ -1,10 +1,12 @@
 package com.github.springwebsocketmongodb.user;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -19,6 +21,9 @@ public class UserService {
     public void disconnect(User user) {
         var storedUser = repository.findById(user.getNickName())
                 .orElse(null);
+
+        log.info("storedUser = " + storedUser);
+
         if (storedUser != null) {
             storedUser.setStatus(Status.OFFLINE);
         }
